5a5cb3661739718886a0c5a6f3cb487fa39d42f7
Judit Vives <jvivesramon@gmail.com>
2023-04-24 20:52:02 +0200

    Add GentlemanStructure interface type

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,39 @@
 import Gentleman from "../Gentleman/Gentleman";
 
 const App = (): JSX.Element => {
+  const gentlemanData = [
+    {
+      id: 1,
+      name: "Bertin Osborne",
+      status: "Alive",
+      profession: "Youtuber",
+      twitter: "@osbourne",
+      picture: "bertin.jpg",
+      alternativeText: "Osbourne pointing at you",
+      selected: true,
+    },
+    {
+      id: 2,
+      name: "The Farytale",
+      status: "RIP",
+      profession: "Influencer",
+      twitter: "pending",
+      picture: "fary.jpg",
+      alternativeText: "The Fary pointing at you",
+      selected: false,
+    },
+    {
+      id: 3,
+      name: "Julius Churchs",
+      status: "Alive",
+      profession: "Java developer",
+      twitter: "@julius_churchs",
+      picture: "julio.jpg",
+      alternativeText: "Churchs pointing at you",
+      selected: true,
+    },
+  ];
+
   return (
     <div className="container">
       <header className="main-header">
---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export interface GentlemanStructure {
+  id: number;
+  name: string;
+  status: string;
+  profession: string;
+  twitter: string;
+  picture: string;
+  alternativeText: string;
+  selected: boolean;
+}
