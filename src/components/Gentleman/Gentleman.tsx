1adfed44fd48720520fddc88fe795b22ce89d6fb
Judit Vives <jvivesramon@gmail.com>
2023-04-24 21:53:08 +0200

    Add Gentleman Card Component and add Gentlemen Data

--- src/components/Gentleman/Gentleman.tsx
+++ src/components/Gentleman/Gentleman.tsx
@@ -1,26 +1,34 @@
-const Gentleman = (): JSX.Element => {
+import { GentlemanStructure } from "../../types/types";
+
+interface GentlemanProps {
+  gentleman: GentlemanStructure;
+}
+
+const Gentleman = ({
+  gentleman: { name, status, profession, twitter, picture, alternativeText },
+}: GentlemanProps): JSX.Element => {
   return (
     <li className="gentleman">
       <div className="gentleman__avatar-container">
         <img
           className="gentleman__avatar"
-          src="img/fary.jpg"
-          alt="The Fary apuntándote con el dedo"
+          src={`img/${picture}`}
+          alt={alternativeText}
         />
         <span className="gentleman__initial">F</span>
       </div>
       <div className="gentleman__data-container">
-        <h2 className="gentleman__name">The Farytale</h2>
+        <h2 className="gentleman__name">{name}</h2>
         <ul className="gentleman__data-list">
           <li className="gentleman__data">
             <span className="gentleman__data-label">Profesión:</span>
-            Influencer
+            {profession}
           </li>
           <li className="gentleman__data">
-            <span className="gentleman__data-label">Estado:</span> RIP
+            <span className="gentleman__data-label">Estado:</span> {status}
           </li>
           <li className="gentleman__data">
-            <span className="gentleman__data-label">Twitter:</span> pendiente
+            <span className="gentleman__data-label">Twitter:</span> {twitter}
           </li>
         </ul>
       </div>
